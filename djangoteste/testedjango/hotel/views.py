1f6fa796125fce261bdc8cf5d8e37b23e1d810ae
s4muraii <viniciusoliveiramoura24@gmail.com>
2024-05-14 08:10:11 -0300

    update

--- djangoteste/testedjango/hotel/views.py
+++ djangoteste/testedjango/hotel/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 
 def homepage(request):
-    return render(request, 'hotel/homepage.html')
+    return render(request, 'homepage.html')
