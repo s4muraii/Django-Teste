d7de72acb31a4617dea727cc296bee3ed9926e0a
s4muraii <viniciusoliveiramoura24@gmail.com>
2024-05-14 10:51:56 -0300

    update

--- djangoteste/testedjango/hotel/admin.py
+++ djangoteste/testedjango/hotel/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import quarto, hotel
 
-# Register your models here.
+admin.site.register(quarto)
+admin.site.register(hotel)
--- djangoteste/testedjango/hotel/models.py
+++ djangoteste/testedjango/hotel/models.py
@@ -1,26 +1,31 @@
 from django.db import models
-import datetime 
+import datetime
 
 TIPOS_QUARTOS = (
     ("SOLTEIRO", "Solteiro"),
-    ("CASAL","Casal"),
-    ("CONFORT","Confort"),
-    ("LUXO","Luxo"),
+    ("CASAL", "Casal"),
+    ("CONFORT", "Confort"),
+    ("LUXO", "Luxo"),
 )
 
+
 class hotel(models.Model):
-    titulo = models.CharField(max_length= 50)
-    desc = models.TextField(max_length= 500)
+    titulo = models.CharField(max_length=50)
+    desc = models.TextField(max_length=500)
+    logo = models.ImageField(upload_to="logo/")
 
     def __str__(self):
         return self.titulo
-    
+
+
 class quarto(models.Model):
-    tipo = models.CharField(max_length= 15, choices=TIPOS_QUARTOS)
+    titulo = models.CharField(max_length=50)
+    tipo = models.CharField(max_length=15, choices=TIPOS_QUARTOS)
     disponibilidade = models.IntegerField()
     valor = models.FloatField(max_length=9999)
     descricao = models.TextField(max_length=255)
-    data_reserva = models.DateTimeField(default= datetime.datetime.now)
+    data_reserva = models.DateTimeField(default=datetime.datetime.now)
+    foto_quarto = models.ImageField(upload_to="quartos/")
 
     def __str__(self):
         return self.tipo
